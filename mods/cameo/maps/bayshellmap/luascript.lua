abf3996476cc09eb70fc45de7c4afb82bb41d5f8
ElPollo315 <gamerchuchito@gmail.com>
2020-11-08 22:15:39 -0300

    CNC Generals adapted to 1.4
    new AI
    More USA Units
    GLA Completed 99%
    China Completed 95%

--- mods/cameo/maps/bayshellmap/luascript.lua
+++ mods/cameo/maps/bayshellmap/luascript.lua
@@ -1,42 +1,21 @@
-ProducedUnitTypes =
-{
-	{ factory = NodRax, types = { "e1", "e3", "e5", "e4" } },
-	{ factory = NodAirstrip, types = { "bggy", "bike", "ltnk", "ftnk", "arty" } },
-	{ factory = NodHelipad, types = { "heli" } },
-	{ factory = CityPD, types = { "citypoliceofficer", "citypolicecar" } },
-	{ factory = CityFD, types = { "cityfirefighter", "cityambulance", "cityfiretruck" } },
-	{ factory = CityFD2, types = { "cityfirefighter", "cityambulance", "cityfiretruck" } },
-	{ factory = ZergHive1, types = { "sczergling", "schydralisk" } },
-	{ factory = ZergHive2, types = { "sczergling", "schydralisk", "scultralisk", "scmutalisk", "scguardian", "scinfestedterran" } },
-	{ factory = ZergHive3, types = { "sczergling", "schydralisk", "scmutalisk" } },
-	{ factory = ZergHive4, types = { "sczergling" } },
-	{ factory = ZergHive5, types = { "sczergling" } },
-	{ factory = SovietWF, types = { "ra2htnk", "ra2apoc", "ra2v3", "ra2htk", "ra2ttnk", "yrschp", "ra2zep", "ra2dron" } },
-	{ factory = SovietRax, types = { "ra2e2", "ra2flakt", "ra2dog", "ra2shk", "ra2deso" } },
-	{ factory = SovietShipyard, types = { "ra2dred", "ra2hyd"} },
-	{ factory = GDIWF, types = { "tsmmch", "tssmech", "tsapc", "tsjugg", "tssonic", "tshvr" } },
-	{ factory = GDIRax, types = { "tse1", "tse2", "tsjumpjet2", "tssonic" } },
-	{ factory = GDIHelipad, types = { "tsorcab", "tsorca" } },
-	{ factory = ChopShop1, types = { "2100scav", "2100trike", "2100jeep", "2100jeepmsl" } },
-	{ factory = ChopShop2, types = { "2100scav", "2100trike", "2100jeep", "2100jeepmsl", "2100camper", "2100tractor" } },
-	{ factory = ChopShop3, types = { "2100bus", "2100ftruk", "2100scavtruck", "2100icecream" } }
-}
-
-ProduceUnits = function(t)
-	local factory = t.factory
-	if not factory.IsDead then
-		local unitType = t.types[Utils.RandomInteger(1, #t.types + 1)]
-		factory.Wait(Actor.BuildTime(unitType))
-		factory.Produce(unitType)
-		factory.CallFunc(function() ProduceUnits(t) end)
-	end
-end
-
-SetupFactories = function()
-	Utils.Do(ProducedUnitTypes, function(production)
-		Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
-	end)
-end
+ProtossSquad = { "sczealot", "sczealot", "scdarktemplar", "scdragoon" }
+ProtossHeavy = { "scarchon", "scdragoon", "scdragoon", "scdragoon", "scdragoon", "schightemplar" }
+CabalSquad = { "tscyborg", "tscyborg", "tscyborg", "tscyborg", "tscyborg", "tscyc2", "tsreaper", "tscyc2", "tsreaper" }
+AlliedSquad = { "2tnk", "2tnk", "1tnk", "1tnk", "rajeep", "raarty" }
+AlliedInf = { "rae1", "rae1", "rae1", "rae1", "rae1", "rae1", "rae3", "rae3" }
+SovietSquad = { "ra2e2", "ra2e2", "ra2e2", "ra2e2", "ra2e2", "ra2e2", "ra2flakt", "ra2flakt", "ra2shk" }
+SovietHeavy = { "3tnk", "3tnk", "ftnk", "ltnk", "ltnk", "4tnk" }
+AlphaSquad = { "2100mvw", "2100mvw", "2100rvw", "2100rvw", "2100rvw", "2100mch" }
+AlphaSquadHeavy = { "2100tch", "2100apt", "2100hpt", "2100tch" }
+CitySquad = { "cityfiretruck", "citypolicecar", "tsdoggie", "tsdoggie", "tsdoggie", "tse3", "tse3", "cityambulance" }
+CitySWAT = { "citypolicecar", "cityswat", "cityswat" }
+JojoChopper = { "scwraith", "scwraith" }
+Kirov = { "scbattlecruiser" }
+Kilgore = { "raheli", "mh60", "mh60" }
+Dred = { "ramsub" }
+Carrier = { "sccarrier" }
+Reavers = { "screaver", "screaver" }
+ShockArmy = { "ra2shk", "ra2shk", "ra2shk", "ra2shk", "ttnk" }
 
 BindActorTriggers = function(a)
 	if a.HasProperty("Hunt") then
@@ -56,41 +35,75 @@ BindActorTriggers = function(a)
 	end
 end
 
-SetCash = function()
-	city.Cash = 100000
-	nod.Cash = 100000
-	ussr.Cash = 100000
-	zerg.Cash = 100000
-	scav.Cash = 100000
-	gdi.Cash = 100000
-	city.Resources = 10000000
-	nod.Resources = 10000000
-	ussr.Resources = 10000000
-	zerg.Resources = 10000000
-	scav.Resources = 10000000
-	gdi.Resources = 10000000
+SendCabalUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(cabal, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendCabalUnits(entryCell, unitTypes, interval) end)
 end
 
-SetWaypoints = function()
-	ZergHive1.RallyPoint = ZergRally.Location
-	ZergHive2.RallyPoint = ZergRally.Location
-	ZergHive3.RallyPoint = ZergRally.Location
-	ZergHive4.RallyPoint = ZergRally.Location
-	CityPD.RallyPoint = CityRally.Location
-	CityFD.RallyPoint = CityRally.Location
-	CityFD2.RallyPoint = CityRally.Location
-	NodAirstrip.RallyPoint = NodWFRally.Location
-	NodRax.RallyPoint = NodRaxRally.Location
-	NodHelipad.RallyPoint = NodHeliRally.Location
-	SovietRax.RallyPoint = SovietRaxRally.Location
-	SovietWF.RallyPoint = SovietWFRally.Location
-	SovietShipyard.RallyPoint = SovietShipRally.Location
-	GDIRax.RallyPoint = GDIRaxRally.Location
-	GDIWF.RallyPoint = GDIWFRally.Location
-	GDIHelipad.RallyPoint = GDIHeliRally.Location
-	ChopShop1.RallyPoint = ScavRally.Location
-	ChopShop2.RallyPoint = ScavRally.Location
-	ChopShop3.RallyPoint = ScavRally.Location
+SendAlliedUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(allies, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendAlliedUnits(entryCell, unitTypes, interval) end)
+end
+
+SendProtossUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(protoss, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendProtossUnits(entryCell, unitTypes, interval) end)
+end
+
+SendSovietUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(ussr, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendSovietUnits(entryCell, unitTypes, interval) end)
+end
+
+Send2100Units = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(alpha, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() Send2100Units(entryCell, unitTypes, interval) end)
+end
+
+SendCityUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(city, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendCityUnits(entryCell, unitTypes, interval) end)
+end
+
+ShuttleDrop = function(entry, hpad)
+	local units = Reinforcements.ReinforceWithTransport(protoss, "scshuttle",
+		Reavers, { entry.Location, hpad.Location + CVec.New(1, 2) }, { entry.Location })[2]
+
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(60), function() ShuttleDrop(entry, hpad) end)
+end
+
+ChronoshiftRedUnits = function()
+	local cells = Utils.ExpandFootprint({ SovietChrono.Location }, false)
+	local units = { }
+	for i = 1, #cells do
+		local unit = Actor.Create("2100tch", true, { Owner = alpha, Facing = 0 })
+		BindActorTriggers(unit)
+		units[unit] = cells[i]
+	end
+	RedChrono.Chronoshift(units)
+	Trigger.AfterDelay(DateTime.Seconds(60), ChronoshiftRedUnits)
 end
 
 ticks = 1250
@@ -99,20 +112,47 @@ speed = 7
 Tick = function()
 	ticks = ticks + 1
 	local t = (ticks + 45) % (360 * speed) * (math.pi / 180) / speed;
-	Camera.Position = viewportOrigin + WVec.New(19200 * math.sin(t), 46080 * math.cos(t), 0)
+	Camera.Position = viewportOrigin + WVec.New(3500 * math.sin(t), 3000 * math.cos(t), 0)
 end
 
 WorldLoaded = function()
-	city = Player.GetPlayer("SimCity")
-	nod = Player.GetPlayer("Nod")
+	cabal = Player.GetPlayer("CABAL")
+	allies = Player.GetPlayer("Allies")
+	protoss = Player.GetPlayer("Protoss")
 	ussr = Player.GetPlayer("Soviets")
-	zerg = Player.GetPlayer("Zerg")
-	scav = Player.GetPlayer("Scavengers")
-	gdi = Player.GetPlayer("GDI")
+	alpha = Player.GetPlayer("Project")
+	city = Player.GetPlayer("SimCity")
 	viewportOrigin = Camera.Position
+
+	Trigger.AfterDelay(DateTime.Seconds(2), ChronoshiftRedUnits)
 	
-	SetCash()
-	SetWaypoints()
-	SetupFactories()
-	Utils.Do(ProducedUnitTypes, ProduceUnits)
+	SendProtossUnits(entry1.Location, ProtossSquad, 20)
+	SendProtossUnits(entry6.Location, ProtossSquad, 20)
+	SendProtossUnits(entry2.Location, ProtossHeavy, 20)
+	SendCabalUnits(entry1.Location, CabalSquad, 5)
+	SendCabalUnits(entry6.Location, CabalSquad, 5)
+	SendAlliedUnits(entry2.Location, AlliedSquad, 15)
+	SendAlliedUnits(entry1.Location, AlliedInf, 15)
+	Send2100Units(entry5.Location, AlphaSquad, 20)
+	Send2100Units(entry4.Location, AlphaSquadHeavy, 20)
+	SendSovietUnits(entry3.Location, SovietSquad, 15)
+	SendSovietUnits(entry4.Location, SovietSquad, 15)
+	SendSovietUnits(entry5.Location, SovietHeavy, 20)
+	SendSovietUnits(entry4.Location, ShockArmy, 30)
+	SendCityUnits(entry3.Location, CitySquad, 10)
+	SendCityUnits(entry5.Location, CitySWAT, 10)
+	Send2100Units(ChopperEntry.Location, JojoChopper, 30)
+
+	ShuttleDrop(ShuttleEntry, ReaverDrop)
+	ShuttleDrop(CarrierEntry, ReaverDrop2)
+	Trigger.AfterDelay(DateTime.Seconds(20), function()
+		Send2100Units(entry5.Location, Kirov, 50) 
+	end)
+	Trigger.AfterDelay(DateTime.Seconds(20), function()
+		SendProtossUnits(CarrierEntry.Location, Carrier, 50) 
+	end)
+	Trigger.AfterDelay(DateTime.Seconds(30), function()
+		SendSovietUnits(DredEntry.Location, Dred, 60) 
+	end)
+	SendAlliedUnits(HeliEntry.Location, Kilgore, 30)
 end
