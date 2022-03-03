8cbe7f964bba5358da4ca723be604fe2f815350f
ElPollo315 <gamerchuchito@gmail.com>
2022-03-02 22:29:08 -0300

    New update, overhaul Generals

--- mods/cameo/mappack/CDT_STUPID/mms.lua
+++
@@ -1,9 +0,0 @@
-GivePlayerMoney = function()
-	Trigger.AfterDelay(1, function()
-		moneyMakers = Map.ActorsInBox(Map.TopLeft, Map.BottomRight, function(actor) return actor.Type == "tower.money" end)
-		for i, v in ipairs(moneyMakers) do
-			Config.player.Cash = Config.player.Cash + 60
-			Media.FloatingText("$60", WPos.New(v.CenterPosition.X, v.CenterPosition.Y, v.CenterPosition.Z), 30, HSLColor.Green)
-		end
-	end)
-end
