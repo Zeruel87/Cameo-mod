485c7eb6271e584cd64839eeadb1a3bcecf979df
tjk-ws <67940579+tjk-ws@users.noreply.github.com>
2025-03-05 22:20:36 +0800

    new IFV trait

---
+++ OpenRA.Mods.Cameo/Traits/Conditions/GrantExternalConditionToTransport.cs
@@ -0,0 +1,67 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Cameo.Traits
+{
+	[Desc("Grant an external condition to the killer.")]
+	public class GrantExternalConditionToTransportInfo : TraitInfo
+	{
+		[FieldLoader.Require]
+		[Desc("The condition to apply. Must be included among the target actor's ExternalCondition traits.")]
+		public readonly string Condition = null;
+
+		public override object Create(ActorInitializer init) { return new GrantExternalConditionToTransport(this); }
+	}
+
+	public class GrantExternalConditionToTransport : INotifyEnteredCargo, INotifyExitedCargo, INotifyKilled
+	{
+		public readonly GrantExternalConditionToTransportInfo Info;
+		int conditionToken = Actor.InvalidConditionToken;
+		Actor Transport;
+		ExternalCondition cargoConditionTrait;
+
+		public GrantExternalConditionToTransport(GrantExternalConditionToTransportInfo info)
+		{
+			Info = info;
+		}
+
+		void INotifyEnteredCargo.OnEnteredCargo(Actor self, Actor cargo)
+		{
+			Transport = cargo;
+			cargoConditionTrait = cargo.TraitsImplementing<ExternalCondition>()
+				.FirstOrDefault(t => t.Info.Condition == Info.Condition && t.CanGrantCondition(self));
+
+			if (cargoConditionTrait != null)
+				conditionToken = cargoConditionTrait.GrantCondition(cargo, self);
+		}
+
+		void INotifyExitedCargo.OnExitedCargo(Actor self, Actor cargo)
+		{
+			if (conditionToken != Actor.InvalidConditionToken
+				&& cargoConditionTrait.TryRevokeCondition(Transport, self, conditionToken))
+			{
+				conditionToken = Actor.InvalidConditionToken;
+				Transport = null;
+			}
+		}
+
+		void INotifyKilled.Killed(Actor self, AttackInfo e)
+		{
+			if (conditionToken != Actor.InvalidConditionToken
+				&& cargoConditionTrait.TryRevokeCondition(Transport, self, conditionToken))
+				conditionToken = Actor.InvalidConditionToken;
+		}
+
+	}
+}
